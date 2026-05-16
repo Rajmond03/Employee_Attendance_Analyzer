88a71bc024086226ae5e34f74c0b0c7905b4704c
Csokor Rajmond <rajmond@gmail.com>
2026-05-16 08:12:31 +0200

    feat: add status checking function

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ INPUT_FILE = "attendance.csv"
 OUTPUT_CSV_FILE = "statuses.csv"
 OUTPUT_JSON_FILE = "overtime_employees.json"
 
+def get_status(hours_worked):
+    if hours_worked < 8:
+        return "underworked"
+    
+    elif hours_worked == 8:
+        return "normal_hours"
+
+    else:
+        return "overtime"
+    
